4904dca2825c4283746ecfbea5952d9ebee7cea4
Lawson Allick <lallick@gmail.com>
2023-01-30 08:48:30 -0500

    Revert to previous commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
+import uniqid from "uniqid";
+import Overview from "./components/Overview";
 import "./App.css";
-import Profile from "./components/Profile";
-import Education from "./components/Education";
-import Skills from "./components/Skills";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: "", id: uniqid() },
+      tasks: [],
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '', 
+        id: uniqid()
+      },
+    });
+  };
+
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        <Profile />
-        <Education />
-        <Skills />
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
